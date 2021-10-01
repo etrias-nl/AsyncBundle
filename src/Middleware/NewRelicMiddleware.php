1fc151b66e36b9fc1abda9404587660cc16cd30b
Roland Franssen <franssen.roland@gmail.com>
2021-10-01 13:23:01 +0200

    trial

--- src/Middleware/NewRelicMiddleware.php
+++ src/Middleware/NewRelicMiddleware.php
@@ -4,16 +4,19 @@ declare(strict_types=1);
 
 namespace Etrias\AsyncBundle\Middleware;
 
+use Ekino\NewRelicBundle\NewRelic\Config;
 use Ekino\NewRelicBundle\NewRelic\NewRelicInteractorInterface;
 use League\Tactician\Middleware;
 
 class NewRelicMiddleware implements Middleware
 {
+    protected Config $config;
     protected NewRelicInteractorInterface $interactor;
     protected bool $running = false;
 
-    public function __construct(NewRelicInteractorInterface $interactor)
+    public function __construct(Config $config, NewRelicInteractorInterface $interactor)
     {
+        $this->config = $config;
         $this->interactor = $interactor;
     }
 
@@ -21,6 +24,10 @@ class NewRelicMiddleware implements Middleware
     {
         if ($this->running) {
             return $next($command);
+        }
+
+        if ($this->config->getName()) {
+            $this->interactor->setApplicationName($this->config->getName(), $this->config->getLicenseKey(), $this->config->getXmit());
         }
 
         $this->interactor->startTransaction(\get_class($command));
